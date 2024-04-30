76a6e3b56d7a69c0efaecba103660e5bb8e71d40
Iain Black <iain@rootisgod.com>
2024-04-30 20:52:52 +0100

    Added very basic textual screen

--- tui/main.py
+++ tui/main.py
@@ -4,11 +4,30 @@ Author:  Iain
 Purpose: Multipass TU
 """
 
-# Files
+# Python Files
 from functions import *
-# Libraries
+# Generic Libraries
 import argparse
 from typing import Dict, Any
+# Textual
+from textual.app import App, ComposeResult
+from textual.widgets import Header, Footer
+
+
+# https://textual.textualize.io/tutorial/
+class mptui(App):
+    """A Textual app to manage multipass."""
+
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets for the app."""
+        yield Header()
+        yield Footer()
+
+    def action_toggle_dark(self) -> None:
+        """An action to toggle dark mode."""
+        self.dark = not self.dark
 
 
 def get_args():
@@ -28,6 +47,8 @@ def main():
     mp_version = get_multipass_version()
     print(f"MP Version is: {mp_version}")
     print("*** Program Ended ***")
+    app = mptui()
+    app.run()
 
 
 if __name__ == '__main__':
