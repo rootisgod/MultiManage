115c700619cec21cb50cd6fac1d0974b4924886d
Iain Black <iain@rootisgod.com>
2025-04-05 14:15:59 +0100

    Version 1.15.0 cleanup (#48)
    * Cursor recommended changes
    
    * cursor improvements
    
    * cursor improvements
    
    * PySimpleGUI went closed source... No hobbyist licence. Wooh! wxpython here we come
    
    * Giving up on this version and starting again

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(
     name='MultiManage',
     version='1.0.0',
-    packages=['pysimplegui'],
+    packages=[''],
     url='https://github.com/rootisgod/MultiManage',
     license='GPL3',
     author='Iain BLack',
     author_email='blog@rootisgod.com',
-    description='A GUI Utility to manage Multipass Instances on Windows, Mac and Linux'
+    description='A GUI and TUI Utility to manage Multipass Instances on Windows, Mac and Linux'
 )
